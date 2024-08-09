5d77f72c4cfedc90174936d0a0cd7eeb61884694
Rinkit Adhana <therinkit@gmail.com>
2024-08-09 18:05:24 +0530

    completed

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
-const playButton = document.getElementById('play-button')
-const pauseButton = document.getElementById('pause-button')
-const stopButton = document.getElementById('stop-button')
-const textInput = document.getElementById('text')
-const speedInput = document.getElementById('speed')
-let currentCharacter
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+const stopButton = document.getElementById("stop-button");
+const textInput = document.getElementById("text");
+const speedInput = document.getElementById("speed");
+let currentCharacter;
 
-playButton.addEventListener('click', ()=> {
-    playText(textInput.value)
-})
-pauseButton.addEventListener('click', pauseText)
-stopButton.addEventListener('click', stopText)
-speedInput.addEventListener('input', ()=>{
-    stopText()
-    playText(utterance.text.substring(currentCharacter))
-})
+playButton.addEventListener("click", () => {
+  playText(textInput.value);
+});
+pauseButton.addEventListener("click", pauseText);
+stopButton.addEventListener("click", stopText);
+speedInput.addEventListener("input", () => {
+  stopText();
+  playText(utterance.text.substring(currentCharacter));
+});
 
 const utterance = new SpeechSynthesisUtterance();
- utterance.addEventListener('end', ()=> {
-    textInput.disabled = false
- })
- utterance.addEventListener('boundary', e=>{
-    currentCharacter = e.charIndex
- })
+utterance.addEventListener("end", () => {
+  textInput.disabled = false;
+});
+utterance.addEventListener("boundary", (e) => {
+  currentCharacter = e.charIndex;
+});
 
 function playText(text) {
-    if(speechSynthesis.paused && speechSynthesis.speaking){
-        return speechSynthesis.resume()
-    }
-    if (speechSynthesis.speaking) return
-    utterance.text = text
-    utterance.rate = speedInput.value || 1
-    textInput.disabled = true
-     speechSynthesis.speak(utterance)
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
+  if (speechSynthesis.speaking) return;
+  utterance.text = text;
+  utterance.rate = speedInput.value || 1;
+  textInput.disabled = true;
+  speechSynthesis.speak(utterance);
 }
-function pauseText(){
-    if(speechSynthesis.speaking) speechSynthesis.pause()
+function pauseText() {
+  if (speechSynthesis.speaking) speechSynthesis.pause();
 }
-function stopText(){
-    speechSynthesis.resume()
-    speechSynthesis.cancel()
+function stopText() {
+  speechSynthesis.resume();
+  speechSynthesis.cancel();
+}
+
+var button = document.getElementById("speech");
+var click = document.getElementById("click");
+
+button.style.display = "none";
+var display = 1;
+
+function hideShow() {
+  if (display == 1) {
+    button.style.display = "block";
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bricolage: ["Bricolage Grotesque", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
